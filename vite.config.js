5d239926fe6aeb7c41a0f2e9156281aae15797c7
Budi Yanto <admin@sekolah.com>
2026-09-24 13:46:12 +0800

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Absensi Guru',
+        short_name: 'Absensi',
+        description: 'Aplikasi Presensi Guru',
+        theme_color: '#4F46E5',
+        background_color: '#F8FAFC',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3652/3652191.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3652/3652191.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
